8b069672059c04d2b87e67b4797feed48b2da7cc
Mia <mikyung.yong@poliot.co.kr>
2022-11-17 13:53:07 +0900

    BLE 스캔 관련 로직 뷰모델로 이동

--- app/src/main/java/com/example/bledot/ble/BleFragment.kt
+++ app/src/main/java/com/example/bledot/ble/BleFragment.kt
@@ -1,8 +1,5 @@
 package com.example.bledot.ble
 
-import android.annotation.SuppressLint
-import android.bluetooth.BluetoothDevice
-import android.bluetooth.le.ScanSettings
 import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
@@ -12,33 +9,17 @@ import androidx.fragment.app.Fragment
 import androidx.fragment.app.activityViewModels
 import androidx.recyclerview.widget.LinearLayoutManager
 import com.example.bledot.R
-import com.example.bledot.adapter.BleAdapter
 import com.example.bledot.data.BleDevice
-import com.example.bledot.data.BleDevice.Companion.fromHashMapList
 import com.example.bledot.databinding.FragmentBleBinding
 import com.example.bledot.util.BleDebugLog
 import com.example.bledot.util.btScanningStatus
-import com.xsens.dot.android.sdk.interfaces.XsensDotScannerCallback
-import com.xsens.dot.android.sdk.models.XsensDotDevice
-import com.xsens.dot.android.sdk.utils.XsensDotScanner
 
 
-class BleFragment : Fragment(), XsensDotScannerCallback {
+class BleFragment : Fragment() {
 
     private val logTag = BleFragment::class.simpleName
     private lateinit var binding: FragmentBleBinding
     private val bleViewModel: BleViewModel by activityViewModels()
-
-    private var mXsScanner: XsensDotScanner? = null
-
-    // 중복 체크되어 담긴 센서리스트
-    private val mScannedSensorList = ArrayList<HashMap<String, Any>>()
-
-    // 센서 데이터 담을 그릇, 즉 배열
-    var mDeviceList = arrayListOf<BleDevice>()
-
-    // 사용할 리사이클러뷰 선언
-    private lateinit var bleAdapter: BleAdapter
 
     override fun onCreateView(
         inflater: LayoutInflater,
@@ -75,85 +56,10 @@ class BleFragment : Fragment(), XsensDotScannerCallback {
             BleDebugLog.d(logTag, "버튼 클릭 후 btStatus: ${btScanningStatus.value}")
             if (btScanningStatus.value == true) {
                 // 스캔 시작
-                initXsScanner()
+                bleViewModel.initXsScanner(requireContext())
             } else { // 스캔 중단
-                stopXsScanner()
+                bleViewModel.stopXsScanner()
             }
         }
-
-    }
-
-    private fun initXsScanner() {
-        BleDebugLog.i(logTag, "initXsScanner-()")
-        mXsScanner = XsensDotScanner(activity?.applicationContext, this)
-        mXsScanner?.setScanMode(ScanSettings.SCAN_MODE_BALANCED)
-        mXsScanner?.startScan()
-    }
-
-    @SuppressLint("MissingPermission")
-    override fun onXsensDotScanned(device: BluetoothDevice, p1: Int) {
-        BleDebugLog.i(logTag, "onXsensDotScanned-()")
-        BleDebugLog.d(logTag, "name: ${device.name}, address: ${device.address}")
-
-        device.let { device ->
-            // Use the mac address as UID to filter the same scan result.
-            var isExist = false
-            for (map in mScannedSensorList) {
-                if ((map["KEY_DEVICE"] as BluetoothDevice).address == device.address) isExist = true
-            }
-
-            if (!isExist) {
-                // The original connection state is Disconnected.
-                // Also set tag, battery state, battery percentage to default value.
-                val map = HashMap<String, Any>()
-                map["KEY_DEVICE"] = device
-                map["KEY_NAME"] = device.name
-                map["KEY_CONNECTION_STATE"] = XsensDotDevice.CONN_STATE_DISCONNECTED
-                map["KEY_TAG"] = ""
-                map["KEY_BATTERY_STATE"] = -1
-                map["KEY_BATTERY_PERCENTAGE"] = -1
-                mScannedSensorList.add(map)
-            }
-        }
-        val list = fromHashMapList(mScannedSensorList) as ArrayList
-
-        // 어답터 인스턴스 생성
-        bleAdapter = BleAdapter()
-        bleAdapter.submitList(list) // 데이터 주입
-
-        // 리사이클러뷰 설정
-        binding.recyclerView.apply {
-            layoutManager = LinearLayoutManager(context, LinearLayoutManager.VERTICAL, false)
-            // 어답터 장착
-            adapter = bleAdapter
-        }
-
-        bleAdapter.listener = { device ->
-            BleDebugLog.d(logTag, "device clicked-()")
-            BleDebugLog.d(logTag, "device : $device")
-        }
-    }
-
-    private fun stopXsScanner() {
-        mXsScanner?.stopScan()
-    }
-
-    private fun sensorListToDeviceList(sensorList: ArrayList<HashMap<String, Any>>): ArrayList<BleDevice> {
-        BleDebugLog.i(logTag, "sensorListToDeviceList-()")
-        // 센서 데이터 변환
-        sensorList.forEach { sensorMap ->
-            val bleDevice = BleDevice(
-                name = sensorMap["KEY_NAME"].toString(), // as String 차이
-                macAddress = sensorMap["KEY_DEVICE"].toString(),
-                connectState = sensorMap["KEY_CONNECTION_STATE"] as Int,
-                tag = sensorMap["KEY_TAG"].toString(),
-                batteryState = sensorMap["KEY_BATTERY_STATE"] as Int,
-                batteryPercent = sensorMap["KEY_BATTERY_PERCENTAGE"] as Int,
-            )
-            mDeviceList.add(bleDevice)
-        }
-        BleDebugLog.d(logTag, "deviceList: $mDeviceList")
-        BleDebugLog.d(logTag, "deviceList.size: ${mDeviceList.size}")
-        return mDeviceList
     }
 }
