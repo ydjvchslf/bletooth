b171c56694c21154f2aa7cad42b511b29f680105
Mia <mikyung.yong@poliot.co.kr>
2022-11-28 09:06:20 +0900

    파일생성 추가

--- app/src/main/java/com/example/bledot/realtime/RealtimeViewModel.kt
+++ app/src/main/java/com/example/bledot/realtime/RealtimeViewModel.kt
@@ -1,22 +1,40 @@
 package com.example.bledot.realtime
 
+import android.annotation.SuppressLint
+import android.util.Log
 import androidx.lifecycle.ViewModel
 import com.example.bledot.App
+import com.example.bledot.BuildConfig
 import com.example.bledot.util.BleDebugLog
 import com.xsens.dot.android.sdk.events.XsensDotData
-import com.xsens.dot.android.sdk.interfaces.XsensDotDeviceCallback
 import com.xsens.dot.android.sdk.interfaces.XsensDotRecordingCallback
-import com.xsens.dot.android.sdk.models.*
+import com.xsens.dot.android.sdk.models.XsensDotDevice
+import com.xsens.dot.android.sdk.models.XsensDotPayload
+import com.xsens.dot.android.sdk.models.XsensDotRecordingFileInfo
+import com.xsens.dot.android.sdk.models.XsensDotRecordingState
 import com.xsens.dot.android.sdk.recording.XsensDotRecordingManager
-import java.util.ArrayList
+import com.xsens.dot.android.sdk.utils.XsensDotLogger
+import java.io.File
+import java.security.AccessController.getContext
+import java.text.SimpleDateFormat
+import java.util.*
 
 class RealtimeViewModel: ViewModel(), XsensDotRecordingCallback {
 
     private val logTag = RealtimeViewModel::class.simpleName
     private var mManager: XsensDotRecordingManager? = null
+    private var internalList: ArrayList<XsensDotRecordingFileInfo>? = null
+    private var byteIds: ByteArray? = null
+    private var xsLogger: XsensDotLogger? = null
+    // A list contains tag and data from each sensor
+    private var mDataList: ArrayList<HashMap<String?, Any>>? = null
+    // A list contains mac address and XsensDotLogger object
+    private var mLoggerList: ArrayList<HashMap<String, Any>>? = null
 
     init {
         BleDebugLog.i(logTag, "init-()")
+        // Remove XsensDotLogger objects from list before start data logging.
+        mLoggerList?.clear()
     }
 
     fun initRecord(xsDevice: XsensDotDevice) {
@@ -93,24 +111,50 @@ class RealtimeViewModel: ViewModel(), XsensDotRecordingCallback {
         // A list of file information can be obtained, one message contains: fileId, fileName, dataSize
         BleDebugLog.d(logTag, "fileList: ${list?.size}")
         list?.forEachIndexed { index, fileInfo ->
-            BleDebugLog.d(logTag, "index: [$index]번째, fileId: ${fileInfo.fileId}, fileName: ${fileInfo.fileName}, dataSize: ${fileInfo.dataSize}, ")
+            BleDebugLog.d(logTag, "index: [$index]번째, fileId: ${fileInfo.fileId}, fileName: ${fileInfo.fileName}, dataSize: ${fileInfo.dataSize}")
+            // internal dataList 할당
+            internalList = list
+            // Set export data format
+            val ids = setExportData()
+            mManager?.selectExportedData(ids)
         }
     }
 
+    @SuppressLint("RestrictedApi")
     override fun onXsensDotDataExported(
-        p0: String?,
-        p1: XsensDotRecordingFileInfo?,
-        p2: XsensDotData?
+        address: String?,
+        XsensDotRecordingFileInfo: XsensDotRecordingFileInfo?,
+        XsensDotData: XsensDotData?
     ) {
-        BleDebugLog.i(logTag, "onXsensDotDataExported-()")
+        BleDebugLog.i(logTag, "onXsensDotDataExported-()111111")
+
+        // Logger 경로
+        val xsLogger =
+            XsensDotLogger(App.context(), 1, 2, "fileName", "TAG", "version0", true, 1, "profileName","version1.0")
+
+//        if (xsLogger == null) {
+//            xsLogger = XsensDotLogger.createRecordingsLogger(
+//                App.context(),
+//                byteIds,
+//                XsensDotRecordingFileInfo?.fileName,
+//                "MIA TAG",
+//                "firmwareVersion",
+//                BuildConfig.VERSION_NAME
+//            )
+//        }
+        xsLogger?.update(byteIds)
     }
 
-    override fun onXsensDotDataExported(p0: String?, p1: XsensDotRecordingFileInfo?) {
-        BleDebugLog.i(logTag, "onXsensDotDataExported-()")
+    override fun onXsensDotDataExported(p0: String?, XsensDotRecordingFileInfo: XsensDotRecordingFileInfo?) {
+        BleDebugLog.i(logTag, "onXsensDotDataExported-()222222")
+        XsensDotRecordingFileInfo.let { fileInfo ->
+            BleDebugLog.d(logTag, "id: [${fileInfo?.fileId}], fileName: ${fileInfo?.fileName} exported!!")
+        }
     }
 
     override fun onXsensDotAllDataExported(p0: String?) {
         BleDebugLog.i(logTag, "onXsensDotAllDataExported-()")
+        BleDebugLog.d(logTag, "모든 파일 exported 완료!!")
     }
 
     override fun onXsensDotStopExportingData(p0: String?) {
@@ -139,7 +183,74 @@ class RealtimeViewModel: ViewModel(), XsensDotRecordingCallback {
     }
 
     fun selectInternalFile() {
+        BleDebugLog.i(logTag, "selectInternalFile-()")
         mManager?.requestFileInfo()
     }
 
+    private fun setExportData(): ByteArray {
+        BleDebugLog.i(logTag, "setExportData-()")
+        val mSelectExportedDataIds = ByteArray(3)
+        mSelectExportedDataIds[0] = XsensDotRecordingManager.RECORDING_DATA_ID_TIMESTAMP
+        mSelectExportedDataIds[1] = XsensDotRecordingManager.RECORDING_DATA_ID_EULER_ANGLES
+        mSelectExportedDataIds[2] = XsensDotRecordingManager.RECORDING_DATA_ID_CALIBRATED_ACC
+        byteIds = mSelectExportedDataIds
+        return mSelectExportedDataIds
+    }
+
+    fun exportFile() {
+        if (internalList != null) {
+            mManager?.startExporting(internalList!!)
+        }
+    }
+
+    @SuppressLint("RestrictedApi")
+    fun createLogger() {
+        BleDebugLog.i(logTag, "createLogger-()")
+        val logger = XsensDotLogger.createMtbLogger(App.context(), "address", "device-tag")
+    }
+
+    fun createFile(xsDevice: XsensDotDevice) {
+        BleDebugLog.i(logTag, "createFile-()")
+        // Remove XsensDotLogger objects from list before start data logging.
+        mLoggerList?.clear()
+
+        val appVersion = BuildConfig.VERSION_NAME
+        val fwVersion = xsDevice.firmwareVersion
+        val address = xsDevice.address
+        val tag = xsDevice.tag.ifEmpty { xsDevice.name }
+        var filename = ""
+
+        // Store log file in app internal folder.
+        // Don't need user to granted the storage permission.
+        val dir: File = App.context().getExternalFilesDir(null)!!
+        filename = dir.absolutePath +
+                    File.separator +
+                    tag + "_" +
+                    SimpleDateFormat(
+                    "yyyyMMdd_HHmmss_SSS",
+                    Locale.getDefault()
+                    ).format(Date()) +
+                    ".csv"
+        BleDebugLog.d(logTag, "파일 [$filename] 생성 완료")
+
+        val logger = XsensDotLogger(
+            App.context(),
+            XsensDotLogger.TYPE_CSV,
+            XsensDotPayload.PAYLOAD_TYPE_COMPLETE_EULER,
+            filename,
+            tag,
+            fwVersion,
+            xsDevice.isSynced,
+            xsDevice.currentOutputRate,
+            xsDevice.filterProfileInfoList[0].name,
+            appVersion
+        )
+
+        // Use mac address as a key to find logger object.
+        val map = HashMap<String, Any>()
+        map["KEY_ADDRESS"] = address
+        map["KEY_LOGGER"] = logger
+        mLoggerList?.add(map)
+
+    }
 }
