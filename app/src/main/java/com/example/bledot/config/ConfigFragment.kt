73da34962ad0834f1dcd3d380f4d14f3f318799d
Mia <mikyung.yong@poliot.co.kr>
2023-01-27 17:01:29 +0900

    일반로그인 로그아웃 추가, 구글 로그아웃 해야함

--- app/src/main/java/com/example/bledot/config/ConfigFragment.kt
+++ app/src/main/java/com/example/bledot/config/ConfigFragment.kt
@@ -1,5 +1,10 @@
 package com.example.bledot.config
 
+import android.annotation.SuppressLint
+import android.app.AlertDialog
+import android.content.Context
+import android.content.Intent
+import android.content.SharedPreferences
 import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
@@ -11,7 +16,12 @@ import androidx.fragment.app.activityViewModels
 import androidx.navigation.NavDirections
 import androidx.navigation.Navigation
 import androidx.navigation.ui.NavigationUI
+import com.example.bledot.App
+import com.example.bledot.App.Companion.prefs
+import com.example.bledot.PreferenceUtil
 import com.example.bledot.R
+import com.example.bledot.activity.before.BeforeActivity
+import com.example.bledot.activity.main.MainActivity
 import com.example.bledot.databinding.FragmentConfigBinding
 import com.example.bledot.util.BleDebugLog
 import com.example.bledot.util.toolbarName
@@ -57,5 +67,43 @@ class ConfigFragment : Fragment() {
         binding.changePwBtn.setOnClickListener {
             Navigation.findNavController(binding.root).navigate(R.id.changePwFragment)
         }
+        // Logout 버튼
+        binding.logoutBtn.setOnClickListener {
+            showDialog("Logout", "Are you sure to logout?")
+        }
+    }
+
+    private fun showDialog(title: String, subTitle: String) {
+        val builder = AlertDialog.Builder(context).apply {
+            setTitle(title)
+            setMessage(subTitle)
+            setPositiveButton("YES") { _, _ ->
+                normalLogout()
+                googleLogout()
+            }
+            setNegativeButton("NO") { _, _ -> }
+        }
+        builder.create().show()
+    }
+
+    @SuppressLint("CommitPrefEdits", "ApplySharedPref")
+    private fun normalLogout() {
+        // 일반로그인 경우, pref 초기화
+        val preEmail = App.prefs.getString("email", "no history")
+        BleDebugLog.d(logTag, "preEmail: $preEmail")
+        if (preEmail != "no history") { // 일반로그인 상태
+            BleDebugLog.d(logTag, "==일반로그인 로그아웃==")
+            val prefs : SharedPreferences? = context?.getSharedPreferences("prefs_name", Context.MODE_PRIVATE)
+            val editor = prefs?.edit()
+            editor?.remove("email")
+            editor?.clear()
+            editor?.commit()
+            activity?.startActivity(Intent(activity, BeforeActivity::class.java))
+            activity?.finish()
+        }
+    }
+
+    private fun googleLogout() {
+        // TODO :: Firebase google sign out
     }
 }
