03ed7437baa667de23da2e1b65d91f1025674752
Mia <mikyung.yong@poliot.co.kr>
2023-03-08 15:11:44 +0900

    전체삭제 후 선택칸 갱신 추가

--- app/src/main/java/com/example/bledot/notupload/NotuploadFragment.kt
+++ app/src/main/java/com/example/bledot/notupload/NotuploadFragment.kt
@@ -1,5 +1,6 @@
 package com.example.bledot.notupload
 
+import android.app.AlertDialog
 import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
@@ -7,6 +8,7 @@ import android.view.ViewGroup
 import androidx.databinding.DataBindingUtil
 import androidx.fragment.app.Fragment
 import androidx.fragment.app.activityViewModels
+import androidx.fragment.app.viewModels
 import com.example.bledot.R
 import com.example.bledot.adapter.DataAdapter
 import com.example.bledot.databinding.FragmentNotUploadedBinding
@@ -16,7 +18,7 @@ class NotuploadFragment: Fragment() {
 
     private val logTag = NotuploadFragment::class.simpleName
     private lateinit var binding: FragmentNotUploadedBinding
-    private val notuploadViewModel: NotuploadViewModel by activityViewModels()
+    private val notuploadViewModel: NotuploadViewModel by viewModels()
     // 사용할 리사이클러뷰 생성
     var dataAdapter = DataAdapter()
 
@@ -75,18 +77,34 @@ class NotuploadFragment: Fragment() {
         // delete 버튼
         binding.deleteBtn.setOnClickListener {
             val dataList = dataAdapter.csvDataList
-            notuploadViewModel.checkSelectedData(dataList, false)
+            notuploadViewModel.checkSelectedData(dataList, false) { }
         }
         // upload 버튼
         binding.uploadBtn.setOnClickListener {
             val dataList = dataAdapter.csvDataList
-            notuploadViewModel.checkSelectedData(dataList, true)
+            notuploadViewModel.checkSelectedData(dataList, true) { result ->
+                if (result) {
+                    showCompleteDialog("Complete", "The data uploaded to the server.")
+                } else {
+                    showCompleteDialog("Warning", "The data failed to upload to the server.")
+                }
+            }
         }
         // delete or upload 후 리사이클러뷰 갱신
         notuploadViewModel.isUpdate.observe(viewLifecycleOwner) { isUpdated ->
             if (isUpdated) {
                 dataAdapter.submitList(notuploadViewModel.localFileList)
                 notuploadViewModel.isUpdate.value = false
+            }
+        }
+        // delete or upload 후 선택칸 갱신
+        notuploadViewModel.localFileListSize.observe(viewLifecycleOwner) { listSize ->
+            if (listSize == 0) {
+                binding.layoutUpper.visibility = View.GONE
+                binding.layoutNoData.visibility = View.VISIBLE
+            } else if (listSize != 0 && listSize != null) {
+                binding.layoutUpper.visibility = View.VISIBLE
+                binding.layoutNoData.visibility = View.GONE
             }
         }
     }
@@ -107,5 +125,20 @@ class NotuploadFragment: Fragment() {
         }
 
         dataAdapter.submitList(dataAdapter.csvDataList)
+    }
+
+    private fun showCompleteDialog(title: String, subTitle: String) {
+        val builder = AlertDialog.Builder(context).apply {
+            setTitle(title)
+            setMessage(subTitle)
+            setCancelable(false)
+            setPositiveButton("Action") { _, _ -> }
+        }
+        builder.create().show()
+    }
+
+    override fun onDestroyView() {
+        super.onDestroyView()
+        viewModelStore.clear()
     }
 }
--- app/src/main/java/com/example/bledot/notupload/NotuploadViewModel.kt
+++ app/src/main/java/com/example/bledot/notupload/NotuploadViewModel.kt
@@ -2,17 +2,24 @@ package com.example.bledot.notupload
 
 import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.ViewModel
+import androidx.lifecycle.viewModelScope
 import com.example.bledot.App
 import com.example.bledot.data.CSVData
+import com.example.bledot.retrofit.RemoteDataSource
 import com.example.bledot.util.BleDebugLog
+import com.example.bledot.util.appIsWorking
+import kotlinx.coroutines.launch
 import java.io.File
 
 class NotuploadViewModel : ViewModel() {
 
     private val logTag = NotuploadViewModel::class.simpleName
+    private val remoteDataSource = RemoteDataSource()
     private var path = ""
     var localFileList = ArrayList<CSVData>()
+    var localFileListSize = MutableLiveData<Int>(null)
     var isUpdate = MutableLiveData(false)
+    var isUploading = MutableLiveData(false)
 
     init {
         BleDebugLog.i(logTag, "init-()")
@@ -33,8 +40,6 @@ class NotuploadViewModel : ViewModel() {
         val directory = File(path)
         val files = directory.listFiles()
 
-        //val filesNameList = ArrayList<String>()
-
         files?.forEach { file ->
             BleDebugLog.d(logTag, "file.name: ${file.name}")
             localFileList.add(CSVData(file.name, false))
@@ -42,11 +47,9 @@ class NotuploadViewModel : ViewModel() {
 
         val dataNum = localFileList.size
         BleDebugLog.d(logTag, "dataNum: $dataNum")
-
-        //unloaded.invoke(dataNum)
     }
     // 선택된 아이템이 뭔지 확인 작업
-    fun checkSelectedData(originalList: ArrayList<CSVData>, cmd: Boolean) {
+    fun checkSelectedData(originalList: ArrayList<CSVData>, cmd: Boolean, uploadCallback: (Boolean) -> Unit) {
         BleDebugLog.i(logTag, "checkSelectedData-()")
 
         val specificValue = true
@@ -62,15 +65,81 @@ class NotuploadViewModel : ViewModel() {
         if (selectedList.size == 0) { return }
 
         if (cmd) {
-            uploadLocalData(selectedList)
+            uploadLocalData(selectedList) {
+                if (it) {
+                    uploadCallback.invoke(true)
+                } else {
+                    uploadCallback.invoke(false)
+                }
+            }
         } else {
             deleteLocalData(selectedList)
         }
     }
 
-    private fun uploadLocalData(selectedList: ArrayList<CSVData>) {
+    private fun uploadLocalData(selectedList: ArrayList<CSVData>, callBack: (Boolean) -> Unit) {
         BleDebugLog.i(logTag, "uploadLocalData-()")
+        viewModelScope.launch {
+            var isSuccess = false
+            isUploading.value = true
+            appIsWorking.value = true
 
+            val directory = File(path)
+            val files = directory.listFiles()
+
+            val remainFileList = arrayListOf<File>()
+
+            files?.forEach { file ->
+                if (file.isFile) {
+                    val fileName = file.name
+                    if (selectedList.any { it.name == fileName }) {
+                        // 업로드 성공
+                        BleDebugLog.d(logTag, "[${file.name}] 업로드 성공!")
+                        isUploading.value = false
+                        appIsWorking.value = false
+                        file.delete()
+                        isSuccess = true
+
+                        // 업로드 실패
+//                        BleDebugLog.d(logTag, "[${file.name}] 업로드 실패")
+//                        isUploading.value = false
+//                        appIsWorking.value = false
+//                        isSuccess = false
+                        /*
+                        remoteDataSource.uploadToServer("abc@naver.com", file) { result ->
+                            if (result) {
+                                BleDebugLog.d(logTag, "[${file.name}] 업로드 성공!")
+                                isUploading.value = false
+                                appIsWorking.value = false
+                                // 업로드 성공 후 데이터 지우기
+                                file.delete()
+                                isSuccess = true
+                            } else {
+                                BleDebugLog.d(logTag, "[${file.name}] 업로드 실패")
+                                isUploading.value = false
+                                appIsWorking.value = false
+                                isSuccess = false
+                            }
+                        }
+                        */
+                    } else {
+                        remainFileList.add(file) // Add the file to the new list
+                    }
+                }
+            }
+
+            localFileList.clear()
+            remainFileList.forEach { file ->
+                localFileList.add(CSVData(file.name, false))
+            }
+
+            BleDebugLog.d(logTag, "업로드 후, 삭제 후 localFileList: $localFileList")
+            localFileListSize.value = localFileList.size
+            isExistData()
+            isUpdate.value = true
+
+            callBack.invoke(isSuccess)
+        }
     }
 
     private fun deleteLocalData(selectedList: ArrayList<CSVData>) {
@@ -102,7 +171,8 @@ class NotuploadViewModel : ViewModel() {
             localFileList.add(CSVData(file.name, false))
         }
 
-        BleDebugLog.d(logTag, "삭제 후 localFileList: $localFileList")
+        BleDebugLog.d(logTag, "삭제 후 localFileList: $localFileList, size: ${localFileList.size}")
+        localFileListSize.value = localFileList.size
         isExistData()
         isUpdate.value = true
     }
