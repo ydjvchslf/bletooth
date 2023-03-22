59c508ceb4e096ec3904ba27a0cf508700b0ff55
Mia <mikyung.yong@poliot.co.kr>
2023-03-22 11:30:18 +0900

    Config 화면에서 비번변경 버튼 visible 조건 수정

--- app/src/main/java/com/example/bledot/config/ConfigViewModel.kt
+++ app/src/main/java/com/example/bledot/config/ConfigViewModel.kt
@@ -2,16 +2,17 @@ package com.example.bledot.config
 
 import androidx.lifecycle.ViewModel
 import androidx.lifecycle.viewModelScope
+import com.example.bledot.App
 import com.example.bledot.data.UserInfoEntity
 import com.example.bledot.retrofit.RemoteDataSource
 import com.example.bledot.util.BleDebugLog
+import com.example.bledot.util.isGoogleUser
 import kotlinx.coroutines.launch
 
 class ConfigViewModel : ViewModel() {
 
     private val logTag = ConfigViewModel::class.simpleName
     private val remoteDataSource = RemoteDataSource()
-    lateinit var crnUserInfo: UserInfoEntity
 
     init {
         BleDebugLog.i(logTag, "init-()")
@@ -21,10 +22,6 @@ class ConfigViewModel : ViewModel() {
         BleDebugLog.i(logTag, "getMyInfo-()")
         viewModelScope.launch  {
             // TODO :: 내 정보 보기 api
-            remoteDataSource.getUserInfo("userId") {
-                crnUserInfo = it
-                userInfo.invoke(it)
-            }
         }
     }
 
@@ -39,13 +36,8 @@ class ConfigViewModel : ViewModel() {
         }
     }
 
-    fun isGoogleUser(): Boolean {
-        BleDebugLog.i(logTag, "parseEmail-()")
-        val words = crnUserInfo.email.split("@")
-        BleDebugLog.d(logTag, "words[1]: ${words[1]}")
-        if (words[1] == "gmail.com") {
-            return true
-        }
-        return false
+    fun isShowPwChange(): Boolean {
+        BleDebugLog.i(logTag, "isShowPwChange-()")
+        return !isGoogleUser(App.prefs.getString("email", "no email"))
     }
 }
