507457b97a711167905a6f8e2f563f8439c8c754
Mia <mikyung.yong@poliot.co.kr>
2023-04-05 18:21:12 +0900

    권한설정 수정

--- app/src/main/java/com/example/bledot/activity/before/BeforeActivity.kt
+++ app/src/main/java/com/example/bledot/activity/before/BeforeActivity.kt
@@ -38,8 +38,8 @@ class BeforeActivity : AppCompatActivity() {
     // 권한
     @RequiresApi(Build.VERSION_CODES.S)
     private val requiredPermissionArray = arrayOf(
-        Manifest.permission.READ_EXTERNAL_STORAGE,
-        Manifest.permission.WRITE_EXTERNAL_STORAGE,
+//        Manifest.permission.READ_EXTERNAL_STORAGE,
+//        Manifest.permission.WRITE_EXTERNAL_STORAGE,
         Manifest.permission.ACCESS_COARSE_LOCATION,
         Manifest.permission.ACCESS_FINE_LOCATION,
         Manifest.permission.BLUETOOTH,
